663ba9c97741db0444547aac4f7cf12e6b7abe84
matheustorres <matheustf2003@gmail.com>
2020-08-18 15:15:10 -0300

    CardView, HStack, ForEach, ZStack - Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello there, World!")
+  var body: some View {
+    HStack {
+      ForEach (0..<4) { index in CardView(isFaceUp: true) }
     }
+    .foregroundColor(Color.orange)
+    .padding()
+    .font(Font.largeTitle)
+  }
+}
+
+struct CardView: View {
+  var isFaceUp: Bool
+  var body: some View{
+    ZStack {
+      if isFaceUp{
+        RoundedRectangle(cornerRadius: 10)
+          .fill(Color.white)
+        RoundedRectangle(cornerRadius: 10)
+          .stroke(lineWidth: 3)
+        Text("👻")
+      } else {
+        RoundedRectangle(cornerRadius: 10)
+          .fill(Color.orange)
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
