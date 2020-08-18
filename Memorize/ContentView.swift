1bd1f6451b771a87ab102ce641ec7d0a4f5c0e34
matheustorres <matheustf2003@gmail.com>
2020-08-18 13:40:17 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Matheus Torres on 18/08/20.
+//  Copyright © 2020 Matheus Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
